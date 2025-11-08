f7f90b9fd185a97ceb57d21e1dff0cff9cd6dc6c
Yeabsira Zerihun <abilak0716@gmail.com>
2025-11-08 16:38:47 +0400

    building event section in home page and nav bar.

---
+++ lib/constants.ts
@@ -0,0 +1,44 @@
+
+export type EventItem = {
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+}
+
+export const events: EventItem[] = [
+  {
+    image:'/images/event1.png',
+    title:'React Summit US 2025',
+    slug:'react-summit-us-2025',
+    location:'San Francisco, CA, USA',
+    date: '2025-11-07',
+    time: '09:00 AM',
+  },
+  {
+    image:'/images/event2.png',
+    title:'React Summit US 2025',
+    slug:'react-summit-us-2025',
+    location:'San Francisco, CA, USA',
+    date: '2025-11-07',
+    time: '09:00 AM',
+  },
+  {
+    image:'/images/event3.png',
+    title:'React Summit US 2025',
+    slug:'react-summit-us-2025',
+    location:'San Francisco, CA, USA',
+    date: '2025-11-07',
+    time: '09:00 AM',
+  },
+  {
+    image:'/images/event4.png',
+    title:'React Summit US 2025',
+    slug:'react-summit-us-2025',
+    location:'San Francisco, CA, USA',
+    date: '2025-11-07',
+    time: '09:00 AM',
+  },
+]
